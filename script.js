ca9b8430910112d671b9ef5f65fb19743369e065
ArianaHernandez <arianahernandez@whsat.org>
2019-05-17 23:38:01 +0000

    DidSceneOne

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 /*global $*/
 /*global results*/
-var scene= 1;
+var scene=1;
 $( "#choiceOne" ).click(function(){
 if(scene=1){
+    $("#story").hide();
     $("#choiceOne").text("Pretend to be unconscious");
+    $("#choiceTwo").text("Fight");
+    $("#caption").text("You're being dragged what do you do?");
+    setTimeout(function(){$("#story").show(); }, 3000);
+}else if(scene=2){
+    $("#story").hide();
+    $("#caption").text("GameOver");
+    setTimeout(function(){$("#caption").show(); }, 3000);
 }
 });
