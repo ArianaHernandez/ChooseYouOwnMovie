95270f92818582555613962040c2e814c4fcb3f3
Ariana Hernandez <arianahernandez@whsat.org>
2019-05-20 16:51:52 +0000

    ButtonChange

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 /*global $*/
 /*global results*/
-var scene=1;
+var scene=0;
 $( "#choiceOne" ).click(function(){
-if(scene=1){
+    scene=scene+1;
+if(scene===1){
     $("#story").hide();
     $("#choiceOne").text("Pretend to be unconscious");
     $("#choiceTwo").text("Fight");
     $("#caption").text("You're being dragged what do you do?");
     setTimeout(function(){$("#story").show(); }, 3000);
-}else if(scene=2){
+
+}else if(scene===2){
     $("#story").hide();
     $("#caption").text("GameOver");
     setTimeout(function(){$("#caption").show(); }, 3000);
